721a5bef7b3df68dd08709e39ac5b0c02b23616e
Don <donvoowright@aol.com>
2015-11-06 20:07:45 -0700

    Update add.java

--- java/add.java
+++ java/add.java
@@ -4,31 +4,29 @@
 *
 */
 public class Index {
-private int firstInt;
-private int secondInt;
-
-/**
-* Creates the Index object
-* @param first int 
-* @param second int 
-*/
-public Index(int first,int second)
-{
-firstInt = first;
-secondInt = second;
-}
-/**
-* Adds the first and second value together
-* @returns the total
-*/
-public int addValues(){
-return firstInt + secondInt;
-
-}
-
-public static void main(String [] argu) {
-Index text = new Index(1,2);
-int total = text.addValues();
-System.out.println(total);
-}
+  private int firstInt;
+  private int secondInt;
+  
+  /**
+  * Creates the Index object
+  * @param first int 
+  * @param second int 
+  */
+  public Index(int first,int second){
+    firstInt = first;
+    secondInt = second;
+  }
+  /**
+  * Adds the first and second value together
+  * @returns the total
+  */
+  public int addValues(){
+    return firstInt + secondInt;
+  }
+  
+  public static void main(String [] argu){
+    Index text = new Index(1,2);
+    int total = text.addValues();
+    System.out.println(total);
+  }
 }

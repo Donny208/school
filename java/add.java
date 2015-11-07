7e81f542b08194dab7c4e19bff02a6eca49a1fcf
Don <donvoowright@aol.com>
2015-11-06 19:49:38 -0700

    Create add.java

---
+++ java/add.java
@@ -0,0 +1,9 @@
+public class Index {
+	public static int addValues(int first, int seconed){ 
+		return first + seconed; 
+	}
+
+	public static void main(String[] args) {
+		System.out.println(addValues(1,2));
+	}
+}

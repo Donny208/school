85e39ebf148cd95cf61b331a5a43e508b8cd94a4
Don <donvoowright@aol.com>
2015-11-06 20:46:19 -0700

    Update add.java

--- java/add.java
+++ java/add.java
@@ -1,32 +1,54 @@
+import java.util.Random;
+
 /**
-* lets you add a value
-* @author Cameron Wright
-*
-*/
+ * lets you add a value
+ * @author Cameron Wright
+ *
+ */
 public class Index {
-  private int firstInt;
-  private int secondInt;
-  
-  /**
-  * Creates the Index object
-  * @param first int 
-  * @param second int 
-  */
-  public Index(int first,int second){
-    firstInt = first;
-    secondInt = second;
-  }
-  /**
-  * Adds the first and second value together
-  * @returns the total
-  */
-  public int addValues(){
-    return firstInt + secondInt;
-  }
-  
-  public static void main(String [] argu){
-    Index text = new Index(1,2);
-    int total = text.addValues();
-    System.out.println(total);
-  }
+	private int firstInt;
+	private int secondInt;
+	private int TotalInt;
+	
+	/**
+	 * Creates the Index object
+	 * @param first int 
+	 * @param second int 
+	 */
+	public Index(int first,int second){
+		firstInt = first;
+		secondInt = second;
+		TotalInt = 0;
+	}
+	/**
+	 * Adds the first and second value together
+	 * @returns the totalInt
+	 */
+	public int addValues(){
+		return TotalInt = firstInt + secondInt;
+	}
+	
+	public int getfirstInt(){
+		return firstInt;
+	}
+	
+	public int getsecondInt(){
+		return secondInt;
+	}
+	
+	public int getTotalInt(){
+		return TotalInt;
+	}
+	
+	public String toString(){
+		return getfirstInt() + " + " + getsecondInt() + " = " + getTotalInt();
+	}
+	
+	public static void main(String [] args) {
+		Random rdm = new Random();
+		
+		Index text = new Index(rdm.nextInt(100)+1,rdm.nextInt(100)+1);
+		text.addValues();
+		System.out.println(text);
+	}
 }

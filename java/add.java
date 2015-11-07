054b16aaab0fd34dc8a5cdc550a7662c96608136
Don <donvoowright@aol.com>
2015-11-06 20:07:04 -0700

    Update add.java

--- java/add.java
+++ java/add.java
@@ -1,9 +1,34 @@
+/**
+* lets you add a value
+* @author Cameron Wright
+*
+*/
 public class Index {
-	public static int addValues(int first, int seconed){ 
-		return first + seconed; 
-	}
+private int firstInt;
+private int secondInt;
 
-	public static void main(String[] args) {
-		System.out.println(addValues(1,2));
-	}
+/**
+* Creates the Index object
+* @param first int 
+* @param second int 
+*/
+public Index(int first,int second)
+{
+firstInt = first;
+secondInt = second;
+}
+/**
+* Adds the first and second value together
+* @returns the total
+*/
+public int addValues(){
+return firstInt + secondInt;
+
+}
+
+public static void main(String [] argu) {
+Index text = new Index(1,2);
+int total = text.addValues();
+System.out.println(total);
+}
 }

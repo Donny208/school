14b1ff93e946537168e439d08565f7df359676ac
Don <donvoowright@aol.com>
2015-11-10 11:44:15 -0700

    Create Turtle_Multiple.py

---
+++ python/Turtle_Multiple.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+s = turtle.Screen()
+s.bgcolor('black')
+s.title('Turtle Stamp')
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.shapesize(2)
+t.color('lightblue')
+t.speed(1)
+t.width(5)

f26e88ded4b1416cc6ebd682c21af08b183fd137
Don <donvoowright@aol.com>
2015-11-10 13:04:27 -0700

    Update SEV.py

--- python/SEV.py
+++ python/SEV.py
@@ -8,4 +8,18 @@ I scored 1000 points
 myscore = 1000
 message = "I scored %s points"
 print(message % myscore)
-print("I am embedding a value of %s into my string")
+print("%s I am embedding a value of %s into my string for %s" %(myscore, 100,'3b'))
+for x in range(100):
+    print('x = %s'%x)
+'''
+joke_text = '%s: a device for findnig furniture in the dark'
+bodypart1 = 'Knee'
+bodypart2 = 'Shin'
+print(joke_text % bodypart1)
+print(joke_text % bodypart2)
+'''
+joke_text = '%s: a device for findnig furniture in the dark'
+bodypart1 = 'Knee'
+bodypart2 = 'Shin'
+print(joke_text % bodypart1)
+print(joke_text % bodypart2)

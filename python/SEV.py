b6c97bcefbafdfd161b0fdb3fa1bdbab1332aba1
Don <donvoowright@aol.com>
2015-11-10 12:57:07 -0700

    Create SEV.py

---
+++ python/SEV.py
@@ -0,0 +1,11 @@
+'''
+embedding values inside of strings
+myscore = 1000
+message = 'I scored $s points'
+print(mssage % score)
+I scored 1000 points
+'''
+myscore = 1000
+message = "I scored %s points"
+print(message % myscore)
+print("I am embedding a value of %s into my string")

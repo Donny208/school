f9b9e8168927ffb851c252411b94d9ad1fa07c8b
Don <donvoowright@aol.com>
2015-11-13 11:47:51 -0700

    Update Turtle_Multiple.py

--- python/Turtle_Multiple.py
+++ python/Turtle_Multiple.py
@@ -9,16 +9,16 @@ t = turtle.Turtle()
 t.shape('turtle')
 t.shapesize(2)
 t.color('lightblue')
-t.speed(1)
-t.width(5)
+t.speed(0)
+t.penup()
 
 t2 = turtle.Turtle()
 t2.shape('turtle')
 t2.shapesize(2)
 t2.color('lightgreen')
-t2.speed(1)
+t2.speed(0)
 t2.width(5)
-
+'''
 t.penup()
 t.forward(100)
 t.pendown()
@@ -54,3 +54,38 @@ t2.forward(25)
 t2.penup()
 t2.forward(25)
 t2.stamp()
+'''
+
+color = ['blue', 'red', 'yellow', 'white', 'purple', 'green', 'magenta', 'cyan', 'orange', 'brown', 'lightblue', 'darkblue']
+def clock(d,x):
+    t.home()
+    t.left(d)
+    t.forward(x-100)
+    t.pendown()
+    t.forward(30)
+    t.penup()
+    t.forward(50)
+    t.left(180)
+    t.stamp()
+
+for x in range(12):
+    t.color(color[x-1])
+    #print(x*30)
+    clock(30*x,300)
+
+
+t2.home()
+t2.forward(275)
+t2.right(100)
+t2.penup()
+t2.forward(75)
+t2.right(10)
+t2.stamp()
+time.sleep(0.1)
+t2.clear()
+t2.forward(60)
+
+while True:
+    t2.right(16)
+    t2.forward(76)
+    time.sleep(0.1)
